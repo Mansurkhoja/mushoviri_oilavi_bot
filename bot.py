1bf1278928a5a7db19b07ee92d9bfcc107d4f55a
Mansurkhoja <mansur.atm@gmail.com>
2022-05-04 20:29:19 +0500

    feat: bot.py and test.py for some tests

--- bot.py
+++ bot.py
@@ -5,12 +5,23 @@ TOKEN = '5162584850:AAHgbosIOJZDrEZnS6x2EhngE3pUWubV_GA'
 
 bot = telebot.TeleBot(TOKEN, parse_mode=None)  # You can set parse_mode by default. HTML or MARKDOWN
 
+arrQA = [
+    {'question': 'что то', 'answer': 'ы'},
+    {'question': 'ллл', 'answer': 'й'},
+    {'question': 'ллыл', 'answer': 'ыфы'}
+]
+
 
 @bot.message_handler(content_types=['text'])
-
 def send_echo(message):
     print(message)
-    bot.reply_to(message, 'Priv:' + message.from_user.username)
+    d = next((x for x in arrQA if
+              x['question'].capitalize().replace(" ", "") == message.text.capitalize().replace(" ", "")),
+             None)
+    if d is None:
+        bot.reply_to(message, '((')
+    else:
+        bot.reply_to(message, d['answer'])
 
 
 bot.polling()
