bb59fdba685c338efe9814eee04aacda45c2ec9d
Mansurkhoja <mansur.atm@gmail.com>
2022-05-25 20:04:49 +0500

    feat: bot.py and test.py for some tests

--- bot.py
+++ bot.py
@@ -1,14 +1,22 @@
 # start bot
 import telebot
 
-TOKEN = '5162584850:AAHgbosIOJZDrEZnS6x2EhngE3pUWubV_GA'
+from constants import TOKEN
 
 bot = telebot.TeleBot(TOKEN, parse_mode=None)  # You can set parse_mode by default. HTML or MARKDOWN
 
+
 arrQA = [
-    {'question': 'что то', 'answer': 'ы'},
-    {'question': 'ллл', 'answer': 'й'},
-    {'question': 'ллыл', 'answer': 'ыфы'}
+    {'question': 'Салом', 'answer': 'Салом'},
+    {'question': 'Пеш аз оиладоршави чиро донистан лозим аст',
+     'answer': '1) вазъияти саломатии якдигар \n2) такрибан характери якдигар \n3) шароити оилавии якдигар \n4) '
+               'хукук ва ухдадории зану шавхар'},
+    {'question': 'Оиди вазъияти саломатии якдигар',
+     'answer': '1.1. Дорои ягон касалии рухи ва чисмони; 1.2. Гузаштани муоинаи духтур; 1.3. Нуксони намудор ва ё '
+               'ноаён (гирифтани забон, олуси, касалии пуст...)'},
+    {'question': 'Оиди донистани характери якдигар',
+     'answer': '2.1. Чи гуна завча/шавхари худро дар оянда дидан мехохед? 2.2.Падар ва модари домод, '
+               'чи гуна муносибатро аз келини худ интизор мешаванд? 2.3. Падар ва модари арус чи?'}
 ]
 
 
@@ -16,7 +24,8 @@ arrQA = [
 def send_echo(message):
     print(message)
     d = next((x for x in arrQA if
-              x['question'].capitalize().replace(" ", "") == message.text.capitalize().replace(" ", "")),
+              x['question'].capitalize().replace(" ", "") ==
+              message.text.capitalize().replace(" ", "").replace("?", "").replace(".", "").replace(":", "")),
              None)
     if d is None:
         bot.reply_to(message, '((')
